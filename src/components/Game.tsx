30c4b5ae2ccb487bc71e22980c04e1cbdade4764
onukichi <onuonu007@gmail.com>
2023-01-06 23:03:02 +0900

    js to tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import '../styles.css';
 
 function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{ value }</button>;
